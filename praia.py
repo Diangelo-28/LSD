4550129df9fcf4fc14f28fbe959b8c4c926c70a3
Diangelo <nicolasmorais.mateus28@gmail.com>
2026-04-08 12:38:56 -0300

    Add files via upload

---
+++ praia.py
@@ -0,0 +1,7 @@
+amigos = input("Você tem amigos?: ")
+sol = input("Está ensolarado?:")
+money = input("O que é good do you have: ")
+if amigos == "sim" and sol =="sim" or money == "sim":
+    print ("Ir para a praia")
+else:
+    print("Não ir para á praia")
