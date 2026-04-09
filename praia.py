51f404f35f1dc14ceabb84bb949a0894ebbe8830
Diangelo <nicolasmorais.mateus28@gmail.com>
2026-04-09 06:09:32 -0300

    Delete praia.py

--- praia.py
+++
@@ -1,7 +0,0 @@
-amigos = input("Você tem amigos?: ")
-sol = input("Está ensolarado?:")
-money = input("O que é good do you have: ")
-if amigos == "sim" and sol =="sim" or money == "sim":
-    print ("Ir para a praia")
-else:
-    print("Não ir para á praia")
